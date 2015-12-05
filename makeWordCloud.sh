17d5456129c4dd04dcf54163946ddad2aab08139
Aditi Raval <stargazer.me@gmail.com>
2015-12-04 22:11:06 -0800

    adding images

---
+++ makeWordCloud.sh
@@ -0,0 +1,15 @@
+username0=$1
+username=$(echo $username0 | awk '{print tolower($0)}')
+
+curl "http://greptweet.com/create.php?id=$username"
+url="http://greptweet.com/u/$username/tweets.txt"
+wget $url
+
+mv tweets.txt tweets.txt.gz
+gunzip tweets.txt.gz
+awk -F '|' '{print $3}' tweets.txt > tmp
+mv tmp tweets.txt
+python makeWordCloud.py
+#mv tweets.txt $username".txt"
+rm tweets.txt
+mv tmp.png $username.png
