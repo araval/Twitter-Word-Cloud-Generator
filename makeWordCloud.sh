18c360fef16cf53e0f45455c9ed7c279ed1ad577
Aditi Raval <stargazer.me@gmail.com>
2015-12-04 22:47:03 -0800

    more images

--- makeWordCloud.sh
+++ makeWordCloud.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+
 username0=$1
 username=$(echo $username0 | awk '{print tolower($0)}')
 
 curl "http://greptweet.com/create.php?id=$username"
-url="http://greptweet.com/u/$username/tweets.txt"
-wget $url
+for i in `seq 1 10`
+    do
+        url="http://greptweet.com/u/$username/tweets.txt"
+        wget $url
+        if [[ "x$?" = "x0" ]]
+            then break
+        fi
+        rm tweets.txt
+        sleep 3
+    done
 
 mv tweets.txt tweets.txt.gz
 gunzip tweets.txt.gz
