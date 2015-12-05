7f5d866a57055f89781c6db537ac537f1dff09eb
Aditi Raval <stargazer.me@gmail.com>
2015-12-05 10:14:56 -0800

    changed color func

--- makeWordCloud.py
+++ makeWordCloud.py
@@ -6,8 +6,8 @@ from scipy.misc import imread
 from cleanTweets import clean_text
 from wordcloud import WordCloud, STOPWORDS
 
-def grey_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+def my_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+    return "hsl(%d, %d%%, %d%%)" % (random.randint(0, 60), 60, random.randint(60, 100))
 
 # Read the text.
 with open('tweets.txt', 'rb') as f:
@@ -21,10 +21,9 @@ stopwords.add("will")
 stopwords.add("u")
 
 wc = WordCloud(max_words=100, stopwords=stopwords, margin=10, random_state=5, width=2000, height=1200).generate(text)
-#               font_path="/home/koroviev/Word Cloud Generator/Arial Unicode MS.ttf").generate(text)
 
 fig = plt.figure(figsize=(32, 20), dpi=100)
-plt.imshow(wc.recolor(color_func=grey_color_func, random_state=1))
+plt.imshow(wc.recolor(color_func=my_color_func, random_state=1))
 
 # Save image:
 outfilename = "tmp.png"
